08743dfb70f02f004bb4a83c76a75482a8ba1dd3
TaoLufula <lifulaj@gmail.com>
2023-02-03 13:12:09 -0500

    Added Elevator Class

---
+++ src/sysc3303_elevator/Elevator.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package sysc3303_elevator;
+
+/**
+ * Elevator Class
+ * 
+ * Class responsible for receiving messages from the Scheduler, process it and then return a complete message.
+ *
+ * @author Tao Lufula, 101164153
+ */
+public class Elevator {
+	
+}
