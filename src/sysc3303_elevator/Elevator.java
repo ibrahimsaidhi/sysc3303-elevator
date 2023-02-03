1bceaf030a2ddc0e066638ac90220af0005e3ce6
TaoLufula <lifulaj@gmail.com>
2023-02-03 13:14:16 -0500

    Implemented method to read data from scheduler, and return a message

--- src/sysc3303_elevator/Elevator.java
+++ src/sysc3303_elevator/Elevator.java
@@ -1,15 +1,54 @@
-/**
- * 
- */
 package sysc3303_elevator;
+
+import java.security.MessageDigest;
+import java.text.MessageFormat;
+import java.util.concurrent.BlockingQueue;
 
 /**
  * Elevator Class
  * 
- * Class responsible for receiving messages from the Scheduler, process it and then return a complete message.
+ * Class responsible for receiving messages from the Scheduler, process it and
+ * then return a complete message.
  *
  * @author Tao Lufula, 101164153
  */
-public class Elevator {
+public class Elevator extends Thread {
+
+	private BlockingQueue<FloorEvent> schedulerToElevatorQueue;
+	private BlockingQueue<FloorEvent> elevatorToScheduler;
+
+
+	
+	/**
+	 * Constructor for Elevator Class
+	 *
+	 * @author Tao Lufula, 101164153
+	 */
+	public Elevator(BlockingQueue<FloorEvent> schedulerToElevatorQueue, BlockingQueue<Message> elevatorToScheduler) {
+
+		this.schedulerToElevatorQueue = schedulerToElevatorQueue;
+		this.elevatorToScheduler = elevatorToScheduler;
+	}
+	
+	
+	public void run() {
+		while (true) {
+			try {
+				
+				FloorEvent event = this.schedulerToElevatorQueue.take();
+				System.out.println("Elevator received message from scheduler.");
+
+				Thread.sleep(1000);
+
+				Message message = new Message("Processing FloorEvent : Done"); //class created in different branch
+				
+				System.out.println("Elevator sending out message to Scheduler");
+				elevatorToScheduler.put(message);
+
+			} catch (InterruptedException e) {
+				System.out.println("Elevator Thread interrupted");
+			}
+		}
+	}
 	
 }
