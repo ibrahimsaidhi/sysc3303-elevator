41206c9fad1e229335ecf1bfc02bbb69a865d0f7
Quinn Parrott <quinn.parrott@gmail.com>
2023-02-04 18:52:50 -0500

    Added all tests

---
+++ src/sysc3303_elevator/AllTests.java
@@ -0,0 +1,10 @@
+package sysc3303_elevator;
+
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasses({ ElevatorTest.class, FloorFormatReaderTest.class })
+public class AllTests {
+
+}
