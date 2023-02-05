ab5947642c62e9beae81745aa8689fb9c5ec7de7
Quinn Parrott <quinn.parrott@gmail.com>
2023-02-04 19:25:40 -0500

    Added Scheduler tests

--- src/sysc3303_elevator/AllTests.java
+++ src/sysc3303_elevator/AllTests.java
@@ -4,7 +4,7 @@ import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
-@SelectClasses({ ElevatorTest.class, FloorFormatReaderTest.class })
+@SelectClasses({ ElevatorTest.class, FloorFormatReaderTest.class, SchedulerTest.class })
 public class AllTests {
 
 }
