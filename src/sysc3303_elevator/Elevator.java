da583d4e31d2776e0d912d770ec3908602d2b67b
TaoLufula <lifulaj@gmail.com>
2023-02-16 19:55:41 -0500

    Made changes to Elevator class

--- src/sysc3303_elevator/Elevator.java
+++ src/sysc3303_elevator/Elevator.java
@@ -1,50 +1,95 @@
 package sysc3303_elevator;
 
-import java.util.concurrent.BlockingQueue;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Elevator Class
  *
- * Class responsible for receiving messages from the Scheduler, process it and
- * then return a complete message.
+ * Class responsible for receiving messages from the Elevator elevatorSubsystem,
+ * process it and then notify the elevatorSubsystem.
  *
  * @author Tao Lufula, 101164153
  */
-public class Elevator implements Runnable {
-
-	private BlockingQueue<FloorEvent> schedulerToElevatorQueue;
-	private BlockingQueue<Message> elevatorToScheduler;
+public class Elevator {
+	private int currentFloor;
+	private boolean isMoving;
+	private boolean isMotorOn;
+	private DoorState doorState;
+	private Direction direction;
+	private ButtonLampState[] buttonLampStates;
+	private List<Integer> requestedFloors;
 
 	/**
 	 * Constructor for Elevator Class
 	 *
-	 * @author Tao Lufula, 101164153
 	 */
-	public Elevator(BlockingQueue<FloorEvent> schedulerToElevatorQueue, BlockingQueue<Message> elevatorToScheduler) {
+	public Elevator(int numberOfFloors) {
 
-		this.schedulerToElevatorQueue = schedulerToElevatorQueue;
-		this.elevatorToScheduler = elevatorToScheduler;
+		this.currentFloor = 1; // Main Floor
+		this.isMoving = false;
+		this.isMotorOn = false;
+		this.doorState = DoorState.CLOSED;
+		this.direction = null;
+		this.requestedFloors = new ArrayList<>();
+
+		this.buttonLampStates = new ButtonLampState[numberOfFloors];
+		Arrays.fill(buttonLampStates, ButtonLampState.OFF);
+	}
+
+	public int getCurrentFloor() {
+		return currentFloor;
+	}
+
+	public void setCurrentFloor(int currentFloor) {
+		this.currentFloor = currentFloor;
+	}
+
+	public boolean isMotorOn() {
+		return isMotorOn;
+	}
+
+	public void setMotorOn(boolean motorOn) {
+		this.isMotorOn = motorOn;
+	}
+
+	public boolean isMoving() {
+		return isMoving;
+	}
+
+	public void setMoving(boolean moving) {
+		isMoving = moving;
+	}
+
+	public DoorState getDoorState() {
+		return doorState;
+	}
+
+	public void setDoorState(DoorState doorState) {
+		this.doorState = doorState;
+	}
+
+	public Direction getDirection() {
+		return direction;
+	}
+
+	public void setDirection(Direction direction) {
+		this.direction = direction;
+	}
+
+	public ButtonLampState[] getButtonLampStates() {
+		return buttonLampStates;
+	}
+
+	public void setButtonLampStates(ButtonLampState[] buttonLampStates) {
+		this.buttonLampStates = buttonLampStates;
 	}
 
 
-	public void run() {
-		while (true) {
-			try {
-
-				FloorEvent event = this.schedulerToElevatorQueue.take();
-				Logger.println("Got message from scheduler.");
-
-				Thread.sleep(1000);
-
-				Message message = new Message("Processing FloorEvent : Done");
-
-				Logger.println("Sending out message to Scheduler");
-				elevatorToScheduler.put(message);
-
-			} catch (InterruptedException e) {
-				Logger.println("Elevator Thread interrupted");
-			}
-		}
+	public Message processFloorEvent(FloorEvent event) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
