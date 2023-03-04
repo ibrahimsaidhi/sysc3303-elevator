e38d3eca4ff1664172f8fe83089a9f7eff17996b
Quinn Parrott <quinn.parrott@gmail.com>
2023-03-04 18:55:26 -0500

    Small naming change

--- src/sysc3303_elevator/networking/BlockingChannelBuilder.java
+++ src/sysc3303_elevator/networking/BlockingChannelBuilder.java
@@ -5,9 +5,9 @@ import java.util.concurrent.BlockingQueue;
 import sysc3303_elevator.Pair;
 
 public class BlockingChannelBuilder {
-	static class SenderInternal<T> implements BlockingSender<T> {
+	public static class SenderWrapper<T> implements BlockingSender<T> {
 		private BlockingQueue<T> queue;
-		public SenderInternal(BlockingQueue<T> queue) {
+		public SenderWrapper(BlockingQueue<T> queue) {
 			this.queue = queue;
 		}
 
@@ -17,9 +17,9 @@ public class BlockingChannelBuilder {
 		}
 	}
 
-	static class ReceiverInternal<T> implements BlockingReceiver<T> {
+	public static class ReceiverWrapper<T> implements BlockingReceiver<T> {
 		private BlockingQueue<T> queue;
-		public ReceiverInternal(BlockingQueue<T> queue) {
+		public ReceiverWrapper(BlockingQueue<T> queue) {
 			this.queue = queue;
 		}
 
@@ -30,6 +30,6 @@ public class BlockingChannelBuilder {
 	}
 
 	public static <T> Pair<BlockingSender<T>, BlockingReceiver<T>> FromBlockingQueue(BlockingQueue<T> queue) {
-		return new Pair<>(new SenderInternal<>(queue), new ReceiverInternal<>(queue));
+		return new Pair<>(new SenderWrapper<>(queue), new ReceiverWrapper<>(queue));
 	}
 }
