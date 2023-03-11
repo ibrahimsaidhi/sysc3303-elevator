1ce3c915d11a30a3a2467a9b1ef04dbe2dd4ed88
Quinn Parrott <quinn.parrott@gmail.com>
2023-03-11 16:33:15 -0500

    whitespace

--- src/sysc3303_elevator/networking/RawMultiplexer.java
+++ src/sysc3303_elevator/networking/RawMultiplexer.java
@@ -8,26 +8,27 @@ import sysc3303_elevator.Pair;
 
 public class RawMultiplexer<S, R> extends BlockingMultiplexer<Integer, S, R> implements Runnable {
 
-	private HashMap<Integer, BlockingSender<S>> senders;
+    private HashMap<Integer, BlockingSender<S>> senders;
     private ManyBlockingReceiver<R> receiver;
     private Thread receiverThread;
 
     public RawMultiplexer(List<Pair<BlockingSender<S>, BlockingReceiver<R>>> channels) {
-		List<Pair<Integer, BlockingReceiver<R>>> elevatorList = new ArrayList<>();
-		this.senders = new HashMap<>();
-		int channel_id = 0;
-		for (var channel: channels) {
-			elevatorList.add(new Pair<>(channel_id, channel.second()));
-			this.senders.put(channel_id, channel.first());
-			channel_id += 1;
-		}
-		this.receiver = new ManyBlockingReceiver<>(elevatorList);
+        List<Pair<Integer, BlockingReceiver<R>>> elevatorList = new ArrayList<>();
+        this.senders = new HashMap<>();
+        int channel_id = 0;
+        for (var channel : channels) {
+            elevatorList.add(new Pair<>(channel_id, channel.second()));
+            this.senders.put(channel_id, channel.first());
+            channel_id += 1;
+        }
+        this.receiver = new ManyBlockingReceiver<>(elevatorList);
         this.receiverThread = new Thread(this.receiver);
     }
 
     @Override
     public void put(TaggedMsg<Integer, S> msg) throws InterruptedException {
-        this.senders.get(msg.id()).put(msg.content());;
+        this.senders.get(msg.id()).put(msg.content());
+        ;
     }
 
     @Override
