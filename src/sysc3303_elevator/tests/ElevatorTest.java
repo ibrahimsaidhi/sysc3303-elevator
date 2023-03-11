969b44f530f9c3ae51e213a12aa6b366f505f65a
Quinn Parrott <quinn.parrott@gmail.com>
2023-03-11 14:25:16 -0500

    Fixed elevator unit test

--- src/sysc3303_elevator/tests/ElevatorTest.java
+++ src/sysc3303_elevator/tests/ElevatorTest.java
@@ -2,27 +2,23 @@ package sysc3303_elevator.tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.lang.Thread.UncaughtExceptionHandler;
-
 import org.junit.jupiter.api.Test;
 
 import sysc3303_elevator.Direction;
+import sysc3303_elevator.ElevatorResponse;
 import sysc3303_elevator.ElevatorSubsystem;
 import sysc3303_elevator.FloorEvent;
-import sysc3303_elevator.Message;
 import sysc3303_elevator.networking.BlockingReceiver;
 import sysc3303_elevator.networking.BlockingSender;
 
 class ElevatorTest {
-	
+
 
 	@Test
 	void test() throws Throwable {
 
 		var event1 = new FloorEvent(null, 2, Direction.Down, 1);
 		var event2 = new FloorEvent(null, 3, Direction.Up, 4);
-		
-		var exception = new RuntimeException();
 
 		var inbound = new BlockingReceiver<FloorEvent>() {
 			public int takeCount = 0;
@@ -36,16 +32,16 @@ class ElevatorTest {
 					return event2;
 				}
 				default:
-					throw exception;
+					throw new InterruptedException();
 				}
-				
+
 			};
 		};
 
-		var outbound = new BlockingSender<Message>() {
+		var outbound = new BlockingSender<ElevatorResponse>() {
 			public int count = 0;
 			@Override
-			public void put(Message e) throws InterruptedException {
+			public void put(ElevatorResponse e) throws InterruptedException {
 				count++;
 			}
 		};
@@ -56,26 +52,15 @@ class ElevatorTest {
 				inbound,
 				outbound
 		);
-		
+
 		var t1 = new Thread(e1, "Elev-Sub");
 
-		var catcher = new UncaughtExceptionHandler() {
-			public Throwable caughtException = null;
-			@Override
-			public void uncaughtException(Thread t, Throwable e) {
-				caughtException = e;
-			}
-		};
-		t1.setDefaultUncaughtExceptionHandler(catcher);
 
 		t1.start();
 		t1.join();
 
-		if (exception != catcher.caughtException) {
-			throw catcher.caughtException;
-		}
 		assertEquals(3, inbound.takeCount);
-//		assertEquals(2, outbound.count);
+		assertEquals(1, outbound.count);
 	}
 
 }
