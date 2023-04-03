1a02cbe254ec6f90ad4ab4e12872e7a5fb90f910
Quinn Parrott <quinn.parrott@gmail.com>
2023-04-03 15:53:45 -0400

    Made elevator queue order src floors before dest floors

--- src/sysc3303_elevator/tests/ElevatorQueueTest.java
+++ src/sysc3303_elevator/tests/ElevatorQueueTest.java
@@ -55,6 +55,94 @@ public class ElevatorQueueTest {
         }, queue.getQueue().toArray());
 
     }
+	@Test
+    public void testDependentSimple() throws InterruptedException {
+        var queue = new ElevatorQueue(2);
+
+        queue.add(5, 2);
+        assertArrayEquals(new Integer[] {
+            5
+        }, queue.getQueue().toArray());
+
+        assertEquals(5, queue.peek().get());
+        queue.next();
+        assertArrayEquals(new Integer[] {
+            2
+        }, queue.getQueue().toArray());
+
+        assertEquals(2, queue.peek().get());
+        queue.next();
+
+        assertEquals(Optional.empty(), queue.peek());
+    }
+
+	@Test
+    public void testDependent() throws InterruptedException {
+        var queue = new ElevatorQueue(2);
+
+        assertEquals(Direction.Down, queue.getDirection());
+        assertArrayEquals(new Integer[] {}, queue.getQueue().toArray());
+
+        queue.add(5, 2);
+        assertEquals(Direction.Up, queue.getDirection());
+        assertArrayEquals(new Integer[] {
+            5
+        }, queue.getQueue().toArray());
+
+        queue.add(0, 5);
+        assertArrayEquals(new Integer[] {
+            5,
+            0
+        }, queue.getQueue().toArray());
+
+        queue.add(3, 1);
+        assertArrayEquals(new Integer[] {
+            3,
+            5,
+            0,
+        }, queue.getQueue().toArray());
+
+        assertEquals(3, queue.peek().get());
+        queue.next();
+        assertArrayEquals(new Integer[] {
+            5,
+            1,
+            0,
+        }, queue.getQueue().toArray());
+
+        assertEquals(5, queue.peek().get());
+        queue.next();
+        assertArrayEquals(new Integer[] {
+            2,
+            1,
+            0,
+        }, queue.getQueue().toArray());
+
+        assertEquals(2, queue.peek().get());
+        queue.next();
+        assertArrayEquals(new Integer[] {
+            1,
+            0,
+        }, queue.getQueue().toArray());
+
+        assertEquals(1, queue.peek().get());
+        queue.next();
+        assertArrayEquals(new Integer[] {
+            0,
+        }, queue.getQueue().toArray());
+
+        assertEquals(0, queue.peek().get());
+        queue.next();
+        assertArrayEquals(new Integer[] {
+            5,
+        }, queue.getQueue().toArray());
+
+        assertEquals(5, queue.peek().get());
+        queue.next();
+        assertArrayEquals(new Integer[] {
+        }, queue.getQueue().toArray());
+        assertEquals(Optional.empty(), queue.peek());
+    }
 
 	@Test
     public void testDown() throws InterruptedException {
