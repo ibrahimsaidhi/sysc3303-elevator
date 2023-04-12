201fcf1dca4834e8d28c1e5ea4d1ab162792d3e0
Quinn Parrott <quinn.parrott@gmail.com>
2023-04-12 15:38:51 -0400

    Implemented performance measuring
    Co-authored-by: Hamza Zafar <Hamzaman10@users.noreply.github.com>

--- src/sysc3303_elevator/Logger.java
+++ src/sysc3303_elevator/Logger.java
@@ -2,14 +2,27 @@ package sysc3303_elevator;
 
 public class Logger {
 	private static final boolean DEBUG = false;
+	private static final boolean PERFORMANCE = false;
 
 	public synchronized static void debugln(String msg) {
+		if (PERFORMANCE) {
+			return;
+		}
 		if (DEBUG) {
-			System.out.println(String.format("%18s: -%s",Thread.currentThread().getName(), msg));
+			System.out.println(String.format("%18s: -%s", Thread.currentThread().getName(), msg));
 		}
 	}
 
 	public synchronized static void println(String msg) {
-		System.out.println(String.format("%18s: %s",Thread.currentThread().getName(), msg));
+		if (PERFORMANCE) {
+			return;
+		}
+		System.out.println(String.format("%18s: %s", Thread.currentThread().getName(), msg));
+	}
+
+	public synchronized static void outputPerf(String msg) {
+		if (PERFORMANCE) {
+			System.out.println(msg);
+		}
 	}
 }
